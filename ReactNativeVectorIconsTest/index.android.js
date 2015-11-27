5aa0867a981702ca7023679665328f540f87ea8d
Shailesh Kumar <shailesh.kumar@outlook.com>
2015-11-28 03:09:54 +0530

    Adding ReactNativeVectorIconsTest 1. Make sure you have 'Fonts' instead of 'fonts' directory inside assets. 2. Make sure to delete the build folder. very important!!!

---
+++ ReactNativeVectorIconsTest/index.android.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+var Icon = require('react-native-vector-icons/Ionicons');
+
+var ReactNativeVectorIconsTest = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Shake or press menu button for dev menu
+        </Text>
+        <Icon name="home" size={30} color="#900" />
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ReactNativeVectorIconsTest', () => ReactNativeVectorIconsTest);
